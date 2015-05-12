9f03439912062d6793c24e2f1bc63ae9be9219aa
Andrew Maier <andrew.maier@gsa.gov>
2015-05-12 17:22:05 -0400

    Topic pages

--- search_results_page.php
+++ search_results_page.php
@@ -5,13 +5,13 @@
     <? include "_includes/_header.php"; ?>
     <main class="h-feed">
       <div class="wrap">
-        <h1 class="p-name">151 Results for <?= $query ?> </h1>
+        <h1 class="p-name">151 Results for <span class="query"><?= $query ?></span> </h1>
         <article id="AF151-06<? echo $i; ?>" class="topic h-entry is-favorite">
-          <div class="topic-content">
+            <a href="topic.php" class="topic-content">
             <h1 class="entry-title">Monopropellant Thrusters for Cubesats</h1>
             <h2>AF151-06<? echo $i; ?></h2>
             <p class="entry-summary">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-          </div>
+          </a>
           <nav>
             <a href="#">View details</a>
             <form action="proposal_progress_report.php"><button>Start a proposal</button></form>
@@ -20,11 +20,11 @@
         </article>
         <? for ($i=0; $i < 6; $i++): ?>
           <article id="AF151-06<? echo $i; ?>" class="topic h-entry">
-            <div class="topic-content">
+            <a href="topic.php" class="topic-content">
               <h1 class="entry-title">Monopropellant Thrusters for Cubesats</h1>
               <h2>AF151-06<? echo $i; ?></h2>
               <p class="entry-summary">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-            </div>
+            </a>
             <nav>
               <a href="#">View details</a>
               <form>
---
+++ topic.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+  <? include "_includes/_head.php"; ?>
+  <body class="topic">
+    <? include "_includes/_header.php"; ?>
+    <main class="topic">
+      <div class="wrap">
+        <header>
+          <h1>Waveform Agile, Low-cost Multi-function Radio Frequency ISR in Contested Environment</h1>
+          <h2>SBIR AF151-145</h2>
+          <nav>
+            <form action="proposal_progress_report.php"><button>Start a proposal</button></form>
+            <form><button class="button-remove">Remove from my topics</button></form>
+          </nav>
+        </header>
+        <section class="topic-objective">
+          <h1>Objective</h1>
+          <p>Develop innovative waveforms and exploitation algorithms for concurrent synthetic aperture radar (SAR) and ground moving target indication (GMTI) in contested environment.</p>
+        </section>
+        <section class="topic-area">
+          <h1>Research &amp; Technical Areas</h1>
+          <p>Electronics</p>
+        </section>
+        <section class="topic-description">
+          <h1>Description</h1>
+          <p>Global integrated intelligence, surveillance, and reconnaissance (GIISR) is one of the service core functions of the U.S. Air Force. GIISR will provide battlefield information to the warfighter at any weather condition in real-time. Experts believe unauthorized users can hold our intelligence, surveillance, and reconnaissance (ISR) assets at risk and force us to operate at sub-optimal standoffs. Further, inexpensive electronic attack systems can limit our ability to collect reliable ISR from mono-static platforms. Hence, to address these onerous technical challenges for contested/anti-access area denial (A2AD) environment, the Air Force must develop affordable, low-cost ISR systems with advanced exploitation capabilities. In general, future ISR systems must be designed so that unauthorized users cannot penetrate to downgrade our ISR capabilities at any critical chain (transmit, receive, and processing) of operation. Our transmit signals must be designed in such a way that an enemy cannot see our presence.</p>
+          <p>Recently, researchers have developed theoretical foundation for spread spectrum coded linear frequency modulation (LFM) transmit signal that has potentials for solving some of the above mentioned technical challenges of the contested environment [1-2]. Some of the characteristics of these signals are: (a) inherit properties of direct sequence spread spectrum and LFM waveforms, (b) remain almost orthogonal on both transmit and receive, and (c) can hide while operating in adversaries environments. In addition, when properly processed, two important benefits of these signals are: (a) can provide exploitation capabilities that LFM waveforms exhibit (i.e., Doppler tolerant) and (b) can achieve high resolution imaging capability to separate a weak target that is close to a strong target. Other approaches on waveforms design and exploitations can be found in various literatures [3-5].</p>
+          <p>We are seeking sensor technology development to accomplish both (1) transmit signal design and (2) exploitation algorithms for simultaneous SAR and GMTI processing. This technology should advance the state-of-the-art and be effective in the A2AD environment; in particular, our signal should not be corrupted by the other users so as to reduce the effectiveness of the surveillance systems. Operating environment to be focused on is contested region but not limited to other scenarios. Performance measures to include are improved signal to clutter and noise ratio, ultra high resolution to discern a weak target close to a strong target, and shorter time for both SAR and GMTI exploitation.</p>
+          <p>The emphasis of this research will be developing waveforms and exploitation algorithms for affordable ISR systems to provide tactical edge in the A2AD/contested environment. We are interested in experimenting proposed waveforms (in hardware) and using these to develop joint SAR and GMTI exploitation algorithms. If implemented successfully, the key benefits to the warfighters will be: (1) transmit signals can hide in adversaries environment but enable ISR objectives and (2) solve the joint SAR and GMTI problem without complexities associated with reconfiguring the radar systems for SAR or GMTI mode. Teaming with a prime contractor is encouraged. No government funded equipment or data will be provided. The contractor will be responsible for the performance evaluation of the waveforms and the exploitation algorithms. No Air Force Research Laboratory facilities will be provided. Government will define the performance parameters. The contractor shall use simulated data (as part of the solution to this research problem) for performance evaluation.</p>
+          <p>PHASE I: Research and develop affordable and practical radar waveforms to address contested environment ISR. Using these waveforms, develop signature exploitation concept for concurrent SAR and GMTI for multiple moving and stationary targets detection, geolocation, and tracking. Develop initial technology transition plan and business case analysis.</p>
+          <p>PHASE II: Implement (hardware and software) the waveforms and signal processing concepts proposed in Phase I for target detection, geolocation, and tracking of moving targets for contested environments. Develop software toolbox to fully characterize performance bounds of the proposed exploitation algorithms. Refine technology transition plan and business case analysis.</p>
+          <p>PHASE III: Qualify and insert proposed technology into an acquisition program.</p>
+        </section>
+        <section class="topic-references">
+          <h1>References</h1>
+          <ol>
+            <li>U. Majumder, M. Bell, M. Rangaswamy, "A Novel Approach for Designing Diversity Radar Waveforms that are Orthogonal on Both Transmit and Receive," Proceedings of IEEE Radar Conference, 2013.</li>
+            <li>U. Majumder, M. Bell, M. Rangaswamy, "Diverse, Waveforms and Processing Techniques for Joint GMTI and SAR Exploitation," to appear in Proceedings of IEEE Radar Conference, 2014.</li>
+            <li>M. Davis, R. Kapfer and R. Bozek, Common Waveform for Simultaneous SAR and GMTI, Proceedings of IEEE Radar Conference, 2011, Kansas City, MO.</li>
+            <li>J-C. Guey., and M. Bell. Diversity Waveform Sets for Delay-Doppler Imaging, IEEE Transaction on Information Theory, Volume 44, No.4, July 1998.</li>
+            <li>M. Minardi and E. Zelnio. Comparison of SAR based GMTI and standard GMTI in a dense target environment, Proceedings of SPIE 6237, Algorithms for Synthetic Aperture Radar Imagery XIII, 62370X, May 17, 2006.</li>
+          </ol>
+        </section>
+        <section class="topic-keywords">
+          <h1>Keywords</h1>
+          <p>orthogonal waveforms, synthetic aperture radar (SAR), intelligence surveillance reconnaissance (ISR), ground moving target indication (GMTI), geolocation</p>
+        </section>
+        <dl>
+          <dt>Solicitation ID:</dt>        
+          <dd>DoD SBIR 2015.1</dd>
+          <dt>Pre-Release Date:</dt>
+          <dd>Dec 12, 2014</dd>
+          <dt>Proposal Submission Begin Date:</dt>
+          <dd>Jan 15, 2015</dd>
+          <dt>Proposal Submission End Date:</dt>
+          <dd>Feb 18, 2015</dd>
+        </dl>
+      </div>
+    </main>
+    <? include "_includes/_footer.php"; ?>
+  </body>
+</html>
