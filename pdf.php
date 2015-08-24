48015de19c56125c104b993d060f52efcebbfa55
amaier <andrewmaier@me.com>
2015-08-24 14:54:53 -0400

    Add styles and markup for pdf view

---
+++ pdf.php
@@ -0,0 +1,318 @@
+<!DOCTYPE html>
+<html>
+  <? include "_includes/_head.php"; ?>
+  <body>
+    <main class="cover-sheet">
+      <div class="wrapper">
+        <header class="cover-sheet--header">
+          <h1>
+            Small Business Innovation Research (SBIR) Program 
+            <span class="cover-sheet">Proposal Cover Sheet</span>
+          </h1>
+          <p>Knowingly and willfully making any false, fictitious, or fraudulent statements or representations may be a felony under the Federal Criminal False Statement Act (18 USC Sec 1001), punishable by a fine of up to $10,000, up to five years in prison, or both.</p>
+          <dl>
+            <dt class="proposal-number">Proposal number</dt>
+            <dd class="proposal-number">F000-000-0000</dd>
+            <dt class="date-submitted">Date submitted</dt>
+            <dd class="date-submitted">11-11-1111</dd>
+          </dl>
+          <span class="due-date">SBIR 15.1 due 25 February 2015</span>
+        </header>
+        <section class="cover-sheet--agency-information">
+          <h1>Agency information</h1>
+          <dl>
+            <dt class="name">Name</dt>
+            <dd class="name">Air Force</dd>
+            <dt class="command">Command</dt>
+            <dd class="command">RI</dd>
+            <dt class="topic-number">Topic number</dt>
+            <dd class="topic-number">AF000-000</dd>
+            <dt class="proposal-title">Proposal Title</dt>
+            <dd class="proposal-title">Lorem ipsum dolor sit amet.</dd>
+          </dl>
+        </section>
+        <section class="cover-sheet--firm-information">
+          <h1>Firm information</h1>
+          <dl>
+            <dt class="name">Firm name</dt>
+            <dd class="name">Test firm, Inc.</dd>
+            <dt class="mailing-address">Mailing address</dt>
+            <dd class="mailing-address">
+              123 Anywhere St. <br />
+              Washington, DC 20009
+            </dd>
+            <dt class="website-address">Website address</dt>
+            <dd class="website-address">http://www.www.com</dd>
+            <dt class="duns">DUNS</dt>
+            <dd class="duns">000000262</dd>
+            <dt class="cage">CAGE</dt>
+            <dd class="cage">00000</dd>
+            <dt class="sba-sbc">SBA SBC</dt>
+            <dd class="sba-sbc">SBC-000000000</dd>
+          </dl>
+          <article class="question question-1">
+            <span class="question--number">1</span> 
+            <span class="question--text">Does your firm allow for the release of its contact information to Economic Development Organizations?</span>
+            <span class="question--answer">Yes</span>
+          </article>
+        </section>
+        <section class="cover-sheet--eligibility">
+          <h1>Eligibility</h1>  
+          <article class="question question-2">
+            <span class="question--number">2</span> 
+            <span class="question--text">Are you a small business as described in section 3.15 of the solicitation, with no more than 500 employees including affiliates? </span>
+            <span class="question--answer">Yes</span>
+          </article>
+          <article class="question question-3">
+            <span class="question--number">3</span>
+            <span class="question--text">Does your business concern meet the ownership and control requirements in 13 CFR 121.702?</span>
+            <span class="question--answer">Yes</span>
+          </article>
+          <article class="question question-4">
+            <span class="question--number">4</span>
+            <span class="question--text">Is 50% or more of your firm owned or managed by a corporate entity?</span>
+            <span class="question--answer">No</span>            
+          </article>
+          <article class="question question-5">
+            <span class="question--number">5</span>
+            <span class="question--text">At a minimum, will two-thirds of the research and/or analytical work in Phase I be carried out by your small business as described in section 4.2 of the solicitation?</span>
+            <span class="question--answer">Yes</span>            
+          </article>
+          <article class="question question-6">
+            <span class="question--number">6</span>
+            <span class="question--text">Is primary employment of the principal investigator with your firm (identified below) as described in section 4.2 of the solicitation?</span>
+            <span class="question--answer">Yes</span>
+          </article>
+          <article class="question question-7">
+            <span class="question--number">7</span>
+            <span class="question--text">Has your firm been convicted of a fraud-related crime involving SBIR and/or STTR funds or found civilly liable for a fraud-related violation involving Federal funds?</span>
+            <span class="question--answer">No</span>
+          </article>
+          <article class="question question-8">
+            <span class="question--number">8</span>
+            <span class="question--text">Has your firm&rsquo;s Principal Investigator (PI) or Corporate Official (CO) listed below, or owner been convicted of a fraud-related crime involving SBIR and/or STTR funds or found civilly liable for a fraud-related violation involving Federal funds?</span>
+            <span class="question--answer">No</span>
+          </article>
+          <article class="question question-9">
+            <span class="question--number">9</span>
+            <span class="question--text">Has your firm met the performance benchmarks in section 3.1 of the solicitation and listed by the SBA on their website as eligible to participate?</span>
+            <span class="question--answer">Yes</span>
+          </article>
+          <article class="question question-10">
+            <span class="question--number">10</span>
+            <span class="question--text">Is your firm registered in the SBA's venture capital database as majority-owned by venture capital operating companies, hedge funds, or private equity firms?</span>
+            <span class="question--answer">No</span>
+          </article>
+          <article class="question question-11">
+            <span class="question--number">11</span>
+            <span class="question--text">Is your firm more than 50% owned by a single Venture Capital Owned Company (VCOC), hedge fund, or private equity firm? NO 12. Is your firm more than 50% owned by multiple business concerns that are VCOCs, hedge funds, or private equity firms?</span>
+            <span class="question--answer">No</span>
+          </article>
+          <article class="question question-12">
+            <span class="question--number">12</span> 
+            <span class="question--text">Is your firm more than 50% owned by multiple business concerns that are VCOCs, hedge funds, or private equity firms?</span>
+            <span class="question--answer">No</span>
+          </article>
+          <p class="note">NOTE: IAW Section 4.2 of the solicitation, firms that do not meet all eligibility requirements will not receive award. IAW Section 4.4 of the solicitation, small businesses that are owned in majority part by multiple VCOCs, hedge funds, or private equity funds are ineligible to submit proposals for opportunities in this solicitation.</p>
+        </section>
+        <section class="cover-sheet--business-information">
+          <h1>Business information</h1>
+          <article class="question question-13">
+            <span class="question--number">13</span>
+            <span class="question--text">Number of employees including all affiliates (average for preceding 12 months):</span>
+            <span class="question--answer">69</span>            
+          </article>
+          <article class="question question-14">
+            <span class="question--number">14</span>
+            <span class="question--text">Is your firm affiliated as set forth in 13 CFR 121.103?</span>
+            <span class="question--answer">No</span>
+            <p>If yes, please provide information below</p>
+            <dl>
+              <dt>Name</dt>
+              <dd>[Firm name]</dd>
+              <dt>Mailing address</dt>
+              <dd>[Mailing address here]</dd>
+              <dt>Number of employees</dt>
+              <dd>[##]</dd>
+            </dl>
+          </article>
+          <article class="question question-15">
+            <span class="question--number">15</span>
+            <span class="question--text">As defined by SBA&hellip;</span>
+            <article class="question">
+                <span class="question--number">A</span>
+                <span class="question--text">Are you a socially or economically disadvantaged small business?</span>
+                <span class="question--answer">No</span>              
+            </article>
+            <article class="question">
+              <span class="question--number">B</span>
+              <span class="question--text">Are you a Woman-Owned small business (WOSB)?</span>
+              <span class="question--answer">No</span>
+            </article>
+            <article class="question">
+              <span class="question--number">C</span>
+              <span class="question--text">Are you a certified HUBZone small business concern?</span>
+              <span class="question--answer">No</span>
+            </article>
+            <article class="question">
+              <span class="question--number">D</span>
+              <span class="question--text">Are you a Service-Disabled Veteran-Owned small business (SDVOSB)?</span>
+              <span class="question--answer">No</span>
+            </article>
+          </article>
+          <article class="question question-16">
+            <span class="question--number">16</span>
+            <span class="question--text">Has the Defense Contracting Audit Agency (DCAA) or other agreed upon agency, performed a review of your accounts or records in connection with government contracts or subcontracts within the past 12 months?</span>
+            <span class="question--answer">Yes</span>
+            <aside>
+              <dl>
+                <dt>Agency Name</dt>
+                <dd>DMCA</dd>
+                <dt>Mailing Address</dt>
+                <dd>123 Anywhere Street</dd>
+                <dd>Orlando, FL 11111</dd>
+                <dt>Auditor</dt>
+                <dd>Someone Important</dd>
+                <dt>Phone</dt>
+                <dd>111-111-1111</dd>
+                <dt>Email Address</dt>
+                <dd>first.last@domain.tld</dd>
+                <dt>Agency for which the review was performed</dt>
+                <dd>N/A&mdash;review of indirect rates</dd>
+              </dl>
+            </aside>
+          </article>
+        </section>
+        <section class="cover-sheet--proposal-information">
+          <h1>Proposal information</h1>
+          <dl>
+            <dt class="proposed-cost">Proposed cost</dt>
+            <dd class="proposed-cost">150,000</dd>
+            <dt class="phase">Phase</dt>
+            <dd class="phase">I</dd>
+            <dt class="duration">Duration</dt>
+            <dd class="duration">9 Months</dd>
+          </dl>
+          <article class="question question-17">
+            <span class="question--number">17</span>
+            <span class="question--text">Does the proposed cost include Discretionary Technical Assistance (DTA)?</span>
+            <span class="question--answer">No</span>
+            <p>If yes, what is the proposed DTA amount?</p>
+            <aside>For any purpose other than to evaluate the proposal, this data except proposal cover sheets shall not be disclosed outside the Government and shall not be duplicated, used or disclosed in whole or in part, provided that if a contract is awarded to this proposer as a result of or in connection with the submission of this data, the Government shall have the right to duplicate, use or disclose the data to the extent provided in the funding agreement. This restriction does not limit the Government's right to use information contained in the data if it is obtained from another source without restriction. This restriction does not apply to routine handling of proposals for administrative purposed by Government support contractors as indicated in Section 5.3 of the solicitation. The data subject to this restriction is contained on the pages of the proposal listed on the line below.</aside>
+            <p>Proprietary information (list page numbers): 1-4, 6-22</p>
+          </article>
+          <article class="question question-18">
+            <span class="question--number">18</span>
+            <span class="question--text">Has a proposal for essentially equivalent work been submitted to other US government agencies or DoD components?</span>
+            <span class="question--answer">No</span>
+            <p>If yes, list the name(s) of the agency or DoD component and if SBIR or STTR, list Topic Number in the space below.</p>
+          </article>
+          <article class="question question-19">
+            <span class="question--number">19</span>                        
+            <span class="question--text">Has a contract been awarded for any of the proposals listed above?</span>
+            <span class="question--answer">No</span>
+            <p>If yes, please provide the contract number:</p>
+          </article>
+          <article class="question question-20">
+            <span class="question--number">20</span>
+            <span class="question--text">Are you submitting assertions in accordance with DFARS 252.227-7017 "Identification and assertions use, release, or disclosure restriction"? See Section 11.5 of the solicitation.</span>
+            <span class="question--answer">Yes</span>
+          </article>
+          <article class="question question-21">
+            <span class="question--number">21</span>            
+            <span class="question--text">Are you proposing research that utilizes human/animal subjects or recombinant DNA as described in section 3.11, 3.12, and 3.13 of the solicitation?</span>
+            <span class="question--answer">No</span>
+          </article>
+          <article class="technical-abstract">
+            <h1>Technical abstract <span class="limit">(Limit your abstract to 200 words with no classified or proprietary information)</span></h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi quaerat ex, totam modi sapiente nostrum consequatur quae, iste incidunt facilis illo perferendis, in sint quas. Ullam repellat ipsam cupiditate tempora.</p>
+          </article>        
+          <article class="anticipated-benefits">
+            <h1>Anticipated Benefits/Potential Commercial Applications of the Research or Development. <span class="no-classified">(No classified or proprietary information)</span></h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi quaerat ex, totam modi sapiente nostrum consequatur quae, iste incidunt facilis illo perferendis, in sint quas. Ullam repellat ipsam cupiditate tempora.</p>
+          </article>        
+          <article class="key-words">
+            <h1>List a maximum of 8 Key Words or phrases, separated by commas, that describe the Project.</h1>
+            <p>Command and Control, Cyber, Data Visualization, Data Analytics, Cyber Operator Decision Support Model, Human Cognition, Decision-Making, Mental Models.</p>
+          </article>
+        </section>
+        <section class="cover-sheet--project-team-information">
+          <h1>Project team information</h1>
+          <article class="question question-22">
+            <span class="question--number">22</span>
+            <span class="question--text">Are teaming partners or subcontractors proposed?</span>
+            <span class="question--answer">Yes</span>
+            <ul>
+              <li>Consultant Dany Dovetail &lt;dany@shoreline.com&gt; </li>
+            </ul>
+          </article>
+          <article class="question question-23">
+            <span class="question--number">23</span>
+            <span class="question--text">Are you proposing to use foreign nationals as defined in section 3.5 of the solicitation for work under the proposed effort?</span>
+            <span class="question--answer">No</span>
+          </article>
+        </section>
+        <section class="cover-sheet--principal-investigator">
+          <h1>Principal Investigator (PI)/Corporate Official (CO) Information</h1>
+          <article class="question question-24">
+            <span class="question--number">24</span>            
+            <span class="question--text">What percentage of the principal investigator's total time will be on the project?</span>
+            <span class="question--answer">23%</span>
+          </article>
+          <article class="question question-25">
+            <span class="question--number">25</span>
+            <span class="question--text">Is the principal investigator (identified below) a woman?</span>
+            <span class="question--answer">No</span>
+          </article>
+          <article class="question question-26">
+            <span class="question--number">26</span>
+            <span class="question--text">Is the principal investigator (identified below) socially/economically disadvantaged?</span>
+            <span class="question--answer">No</span>
+          </article>
+          <article class="question question-27">
+            <span class="question--number">27</span>            
+            <span class="question--text">Is your firm's PI, CO, or owner, a faculty member or student of an institution of higher education?</span>
+            <span class="question--answer">No</span>
+          </article>
+        </section>
+        <section class="cover-sheet--affiliated-persons">
+          <h1>Affiliated persons</h1>
+          <article class="person">
+            <h1>Project Manager/Principal Investigator</h1>
+            <dl>
+              <dt>Name</dt>
+              <dd>Dr. Paul Scrivens</dd>
+              <dt>Title</dt>
+              <dd>Principal Investigator</dd>
+              <dt>Phone</dt>
+              <dd>111-111-1111 Ext: 1111</dd>
+              <dt>Fax</dt>
+              <dd>111-111-1111</dd>
+              <dt>Email</dt>
+              <dd>a@principalinvestiga.tor</dd>
+            </dl>
+            <div class="signature">Signature</div>
+            <div class="date">Date</div>
+          </article>
+          <article class="person">
+            <h1>Corporate official</h1>
+            <dl>
+              <dt>Name</dt>
+              <dd>Mr. Peter B. Dyson</dd>
+              <dt>Title</dt>
+              <dd>Chairman &amp; CEO</dd>
+              <dt>Phone</dt>
+              <dd>111-111-1111 Ext: 1111</dd>
+              <dt>Fax</dt>
+              <dd>111-111-1111</dd>
+              <dt>Email</dt>
+              <dd>a@corporateoffic.ial</dd>
+            </dl>
+            <div class="signature">Signature</div>
+            <div class="date">Date</div>
+          </article>
+        </section>
+      </div>
+    </main>
+  </body>
+</html>
